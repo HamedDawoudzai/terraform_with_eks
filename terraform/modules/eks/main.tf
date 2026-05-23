e22c47730442d8474e6007319ac197bd939de172
Hamed Dawoudzai <hamed.dawoudzai@gmail.com>
2026-05-23 18:12:24 -0400

    fixed critical deployment issues, added security so no one can run destroy etc etc

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -40,6 +40,30 @@ resource "aws_security_group" "worker" {
   }
 }
 
+# --- Launch Template (attaches worker SG + configures disk) ---
+
+resource "aws_launch_template" "worker" {
+  name_prefix = "${var.cluster_name}-worker-"
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size           = 20
+      volume_type           = "gp3"
+      delete_on_termination = true
+    }
+  }
+
+  vpc_security_group_ids = [aws_security_group.worker.id]
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "${var.cluster_name}-worker"
+    }
+  }
+}
+
 # --- Managed Node Group ---
 
 resource "aws_eks_node_group" "default" {
@@ -47,9 +71,12 @@ resource "aws_eks_node_group" "default" {
   node_group_name = "${var.cluster_name}-nodes"
   node_role_arn   = var.worker_role_arn
   subnet_ids      = var.subnet_ids
+  instance_types  = [var.node_instance_type]
 
-  instance_types = [var.node_instance_type]
-  disk_size      = 20
+  launch_template {
+    id      = aws_launch_template.worker.id
+    version = aws_launch_template.worker.latest_version
+  }
 
   scaling_config {
     desired_size = var.node_desired_size
