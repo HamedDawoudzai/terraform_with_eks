7b3a84f97669e2a4a9c8c40198c0c1229ed37f76
Hamed Dawoudzai <hamed.dawoudzai@gmail.com>
2026-05-23 18:24:12 -0400

    move s3 bucket details to secret

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket         = "hameds-eks-terraform-state"
-    key            = "eks/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "hameds-eks-terraform-locks"
-    encrypt        = true
-  }
+  backend "s3" {}
 }
